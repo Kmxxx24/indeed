dad08d0e3b616322dafa1582cc8b54cb4dbfc996
Kmxxx24 <122954922+Kmxxx24@users.noreply.github.com>
2023-01-27 11:56:29 +0700

    first commit

---
+++ Test Suites/TS101.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS101</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1b53acaf-818e-4eb0-b298-4c3e6db249c4</testSuiteGuid>
+   <testCaseLink>
+      <guid>63ddc4d1-8eab-4c58-ac70-6ecb4e492508</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc103 Search Exception</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
